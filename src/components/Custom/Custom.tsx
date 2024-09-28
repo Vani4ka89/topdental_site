08a0806fd53159f7969cc208a0cce11c562fb129
vanko <ivan.tym4ak@gmail.com>
2024-09-28 18:42:23 +0300

    fixed Custom.tsx added scroll on button

--- src/components/Custom/Custom.tsx
+++ src/components/Custom/Custom.tsx
@@ -4,10 +4,15 @@ import {Link} from "react-router-dom";
 import '../../styles/custom.css';
 
 const Custom: FC = () => {
+    const handleScroll = () => {
+        const element = document.getElementById("contact");
+        element?.scrollIntoView({behavior: 'smooth'});
+    };
+
     return (
         <section className="custom">
             <div className="custom__container">
-                <Link to='#contact' className="btn">
+                <Link to="#" onClick={handleScroll} className="btn">
                     Записатись на прийом →
                 </Link>
             </div>
