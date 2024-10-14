0b105a7b9c0a61f5a3c31dc5d1038f56b608e566
vanko <ivan.tym4ak@gmail.com>
2024-10-14 14:19:46 +0300

    fixed main-layout.css, MainLayout.tsx

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -5,6 +5,26 @@ import {Footer, Header} from "../../components";
 import './main-layout.css';
 
 const MainLayout: FC = () => {
+
+    function isInViewport(element: any) {
+        const rect = element.getBoundingClientRect();
+        return (
+            rect.top < window.innerHeight && rect.bottom >= 0
+        );
+    }
+
+    function handleScroll() {
+        const elements = document.querySelectorAll('h2');
+        elements.forEach(element => {
+            if (isInViewport(element)) {
+                element.classList.add('visible');
+            }
+        });
+    }
+
+    // document.addEventListener('DOMContentLoaded', handleScroll);
+    window.addEventListener('scroll', handleScroll);
+
     return (
         <div className="wrapper">
             <Header/>
